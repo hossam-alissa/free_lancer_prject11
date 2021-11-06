c98e50749422be95daf335b1112596ebff3d00db
hossam.alissa <hossam.alissa@yahoo.com>
2021-11-06 19:09:34 +0200

    ADD New NavScreen

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({Key? key}) : super(key: key);
+
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
