20f6f76702e902ce59872008c4e22087e1d381e8
hossam.alissa <hossam.alissa@yahoo.com>
2021-11-09 01:20:07 +0200

    test logIn

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,46 +13,6 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-
-    // Column(
-    //   children: [
-    //     const SizedBox(height: 20.0),
-    //     Row(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: [
-    //         Container(
-    //           height: 90.0,
-    //           width: 90.0,
-    //           decoration: BoxDecoration(
-    //             borderRadius: BorderRadius.circular(100),
-    //             gradient: const LinearGradient(
-    //               begin: Alignment.topCenter,
-    //               end: Alignment.bottomCenter,
-    //               colors: [
-    //                 Colors.lightBlueAccent,
-    //                 Colors.blue,
-    //               ],
-    //             ),
-    //             boxShadow: [
-    //               BoxShadow(
-    //                 color: Colors.lightBlueAccent.withOpacity(0.2),
-    //                 spreadRadius: 20,
-    //                 blurRadius: 25,
-    //                 offset: const Offset(0, 23), // changes position of shadow
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //     const SizedBox(height: 20.0),
-    //     const Text(
-    //       "Create a \nNew Account",
-    //       style: TextStyle(fontSize: 30.0, color: Colors.white),
-    //     ),
-    //   ],
-    // ),
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
